33bd33664c449da42748bbfb998c7e425683c28e
Arda <ardaboluk2@gmail.com>
2017-10-02 19:31:37 +0300

    Exercise 4.2 is in progress.

--- sutton/exercise_4_2.py
+++ sutton/exercise_4_2.py
@@ -21,23 +21,36 @@ class PolicyIteration:
 
         self.__numStates = p_transitions.shape[0]
         self.__numActions = p_transitions.shape[1]
-        
-        self.__policy_probs = np.ones(self.__numStates, self.__numActions) / self.__numActions        
 
-    def __evaluatePolicy(p_values, p_policy_probs):
+        # Probabilities of transitions for the current policy.
+        self.__policy_probs = np.ones(self.__numStates, self.__numActions) / self.__numActions
+        # Values of states. Vector of shape (numStates)
+        self.__s_values = np.zeros(self.__numStates)
+
+    def __evaluatePolicy():
         """Performs policy evaluation and calculates approximate state-values for a given policy.
-        p_values: Values of states. Vector of shape (numStates).
-        p_policy_probs: Probabilites of taking actions in states. Matrix of shape (numStates, numActions)."""
+        p_values: """
 
-        delta = 0
+        delta = 100
         theta = 0.01
 
         while delta > theta:
 
-            for i in range(0, p_values.shape[0]):
+            delta = 0
 
-                temp = p_values[i]
+            for i in range(0, self.__numStates):
+
+                temp = self.__s_values[i]
                 p_values[i] = np.multiply(np.tile(self.__policy_probs[i,:], (1,1,self.__numStates)), self.__trans[i,:,:], (self.__rewards[i,:,:] + self.__gamma * np.tile(1,self.__numActions,1)))
                 delta = np.maximum(delta, np.abs(temp - p_values[i]))
-                
+
+    def __improvePolicy():
+        """Performs policy improvement on the current policy."""
+
+        policy_stable = True
+
+        for i in range(0, self.__numStates):
+
+            temp = np.argmax(self.__policy_probs[i])
+        
                 
