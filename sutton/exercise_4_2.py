6e306c7521ddf6473b837ee0f9278809295cd031
ardaboluk <ardaboluk2@gmail.com>
2017-10-02 22:27:23 +0300

    Exercise 4.2 is in progress.

--- sutton/exercise_4_2.py
+++ sutton/exercise_4_2.py
@@ -3,6 +3,14 @@
 
 import numpy as np
 
+# set the environment
+
+transitions = np.ones(21,5,21)
+rewards = np.zeros(21,5,21)
+gamma = 0.9
+
+
+
 class PolicyIteration:
     """Class for policy iteration. Contstructor takes transition probabilities and rewards as arguments.
     Both arguments of the constructor are 3-D vectors whose indexes represent state, action and new state, respectively.
@@ -15,17 +23,19 @@ class PolicyIteration:
             if i != 1:
                 raise ValueError("Exception: probabilities of transitions doesn't add up to 1")
 
-        self.__trans = p_transitions
+        self.__transitions = p_transitions
         self.__rewards = p_rewards
         self.__gamma = p_gamma
 
         self.__numStates = p_transitions.shape[0]
         self.__numActions = p_transitions.shape[1]
 
-        # Probabilities of transitions for the current policy.
-        self.__policy_probs = np.ones(self.__numStates, self.__numActions) / self.__numActions
+        # Policy is indexes of actions for each state. Vector of shape (numStates)
+        self.__policy = np.zeros(self.__numStates)
         # Values of states. Vector of shape (numStates)
         self.__s_values = np.zeros(self.__numStates)
+        # useful in checking if the policy has been improved further
+        self.__policy_stable = False
 
     def __evaluatePolicy():
         """Performs policy evaluation and calculates approximate state-values for a given policy.
@@ -41,16 +51,28 @@ class PolicyIteration:
             for i in range(0, self.__numStates):
 
                 temp = self.__s_values[i]
-                p_values[i] = np.multiply(np.tile(self.__policy_probs[i,:], (1,1,self.__numStates)), self.__trans[i,:,:], (self.__rewards[i,:,:] + self.__gamma * np.tile(1,self.__numActions,1)))
+                p_values[i] = np.sum(np.multiply(self.__transitions[i, self.__policy[i], :], (self.__rewards[i, self.__policy[i], :] + self.__gamma * self.__s_values)))
                 delta = np.maximum(delta, np.abs(temp - p_values[i]))
 
     def __improvePolicy():
         """Performs policy improvement on the current policy."""
 
-        policy_stable = True
+        self.__policy_stable = True
 
         for i in range(0, self.__numStates):
 
-            temp = np.argmax(self.__policy_probs[i])
-        
+            temp = self.__policy[i]
+            self.__policy[i] = np.argmax(np.sum(np.multiply(self.__transitions[i,:,:], (self.__rewards[i,:,:] + self.__gamma * np.tile(self.__s_values, (self.__numActions,1)))), axis = 2))
+            if temp != self.__policy[i]:
+                self.__policy_stable = False
+            
                 
+    def optimize():
+        """Finds the approximate optimal policy for a given MDP."""
+
+        while self.__policy_stable == False:
+
+            self.__evaluatePolicy()
+            self.__improvePolicy()
+
+        return [self.__s_values, self.__policy]
