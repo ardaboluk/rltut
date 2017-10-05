df984f9dd16a3d021dacb4a2079f5439cc5cdd2f
Arda <ardaboluk2@gmail.com>
2017-10-05 14:08:42 +0300

    Added grid world example 4.1 in chapter 4

--- sutton/PolicyIteration.py
+++ sutton/PolicyIteration.py
@@ -1,15 +1,24 @@
 
 # Policy Iteration for Chapter 4 (Dynamic Programming)
 
+"""
+env shold have the following methods and fields:
+numStates: the number of states.
+numActions: the number of actions.
+gamma: discount factor for the environment.
+getProbability(s,a,sprime): returns the probabiliy of transition from state s to state sprime via action a.
+getReward(s,a,sprime): returns the reward of going from state s to state sprime via action a.
+"""
+
 import numpy as np
 
 def __checkTransitions(env):
     """Checks if transitions add up to 1 for each state-action pair, returns False."""
-
-    for s in range(0,env.numStates()):
-        for a in range(0,env.numActions()):
+    
+    for s in range(0,env.numStates):
+        for a in range(0,env.numActions):
             sumProbs = 0                
-            for sprime in range(0,env.numStates()):
+            for sprime in range(0,env.numStates):
                 sumProbs += env.getProbability(s,a,sprime)
                 
             if sumProbs != 1:
@@ -25,11 +34,11 @@ def __evaluatePolicy(env, values, policy):
         
         delta = 0
         
-        for s in range(0, env.numStates()):
+        for s in range(0, env.numStates):
             
             temp = values[s]
             newValue = 0
-            for sprime in range(0, env.numStates()):
+            for sprime in range(0, env.numStates):
                 newValue += env.getProbability(s,policy[s],sprime) * (env.getReward(s,policy[s],sprime) + env.gamma * values[sprime])
                 
             values[s] = newValue
@@ -42,15 +51,15 @@ def __improvePolicy(env, values, policy):
 
     policy_stable = True
 
-    for s in range(0, env.numStates()):
+    for s in range(0, env.numStates):
         
         temp = policy[s]
         
         maxAction = 0
         maxActionValue = -float("inf")
-        for a in range(0,env.numActions()):
+        for a in range(0,env.numActions):
             newValue = 0
-            for sprime in range(0, env.numStates()):
+            for sprime in range(0, env.numStates):
                 newValue += env.getProbability(s,a,sprime) * (env.getReward(s,a,sprime) + env.gamma * values[sprime])
             if newValue > maxActionValue:
                 maxActionValue = newValue
@@ -69,8 +78,8 @@ def iteratePolicy(env):
         raise ValueError("Probabilities of state transitions don't add up to 1.")
 
     # initialize variables
-    values = np.zeros(env.numStates())
-    policy = np.zeros(env.numStates())
+    values = np.zeros(env.numStates)
+    policy = np.zeros(env.numStates)
 
     while True:
 
