3aa3943d866c1abc84ba133b7ca76d48b426d49c
Arda <ardaboluk2@gmail.com>
2017-10-10 08:17:41 +0300

    JacksCarRental bugfix in progress.

--- sutton/JacksCarRental.py
+++ sutton/JacksCarRental.py
@@ -39,24 +39,30 @@ class JacksCarRentalEnvironment:
         # prob shows the probability of i cars being returned previous day and j cars being requested today.
 
         # number of cars at the end of the previous day for each location
-        values1 = np.tile(numLoc1,(21,21))
-        values2 = np.tile(numLoc2,(21,21))
+        values1 = np.tile(numLoc1,(21,21,21))
+        values2 = np.tile(numLoc2,(21,21,21))
 
         # number of cars that are returned the previous day, which become available today
         # this should come before action, because we know the number of cars that will be available
         # before performing the action
-        meshRow = np.tile(np.array([range(0,21)]).T,(1,21))
-        values1 = np.clip(values1+meshRow,0,20)
-        values2 = np.clip(values2+meshRow,0,20)
+        meshRow1 = np.tile(np.array([range(0,21)]).T,(1,21))
+        meshRow1 = np.tile(meshRow1,(21,1,1))
+        meshRow2 = np.rot90(meshRow1.T, k = -1)
+        values1 = np.clip(values1+meshRow1,0,20)
+        values2 = np.clip(values2+meshRow2,0,20)
 
         # number of cars after performing the action (if possible)
-        if a < 5:
-            cond = np.logical_and(np.tile(numLoc2,(21,21))+(a-5)>=0,values1-(a-5)<=20)
-        else:
-            cond = np.logical_and(np.tile(numLoc1,(21,21))-(a-5)>=0,values2+(a-5)<=20)
+        if a < 5 and numLoc2 + (a-5) >= 0:
+            cond = values1 - (a-5) <= 20
+            values1[cond] -= a-5
+            values2[cond] += a-5
+        elif a > 5 and numLoc1 - (a-5) >= 0:
+            cond = values2 + (a-5) <= 20
+            values1[cond] -= a-5
+            values2[cond] += a-5
 
-        values1[cond] = values1[cond]-(a-5)
-        values2[cond] = values2[cond]+(a-5)
+        #values1[cond] = values1[cond]-(a-5)
+        #values2[cond] = values2[cond]+(a-5)
             
         # cond1 = np.logical_and(np.tile(numLoc1,(21,21))-(a-5)>=0,values1-(a-5)<=20)
         # cond2 = np.logical_and(np.tile(numLoc2,(21,21))+(a-5)>=0,values2+(a-5)<=20)
@@ -64,14 +70,16 @@ class JacksCarRentalEnvironment:
         # values2[np.logical_and(cond1,cond2)] = values2[np.logical_and(cond1,cond2)]+(a-5)
 
         # number of cars after rental requests
-        meshCol = np.tile(np.array([range(0,21)]),(21,1))
-        values1AfterReq = np.clip(values1-meshCol,0,20)        
-        values2AfterReq = np.clip(values2-meshCol,0,20)
+        meshCol1 = np.tile(np.array([range(0,21)]),(21,1))
+        meshCol1 = np.tile(meshCol1,(21,1,1))
+        meshCol2 = meshCol1
+        values1AfterReq = np.clip(values1-meshCol1,0,20)        
+        values2AfterReq = np.clip(values2-meshCol2,0,20)
 
         # probabilities of location 1 having the values1 numbers of cars
-        prob1 = np.multiply((((3**meshRow)/factorial(meshRow)) * math.exp(-3)), (((3**meshCol)/factorial(meshCol)) * math.exp(-3)))
+        prob1 = np.multiply((((3**meshRow1)/factorial(meshRow1)) * math.exp(-3)), (((3**meshCol1)/factorial(meshCol1)) * math.exp(-3)))
         # probabilities of location 2 having the values2 numbers of cars
-        prob2 = np.multiply((((2**meshRow)/factorial(meshRow)) * math.exp(-2)), (((4**meshCol)/factorial(meshCol)) * math.exp(-4)))
+        prob2 = np.multiply((((2**meshRow2)/factorial(meshRow2)) * math.exp(-2)), (((4**meshCol2)/factorial(meshCol2)) * math.exp(-4)))
 
         # rewards shows the rewards at each location if #row car returned previous day and #col car rented today
         reqDiff1 = values1 - values1AfterReq
