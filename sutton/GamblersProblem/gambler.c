537bd316b86bada4335925e1b66d85ce5a56a8fc
ardaboluk <ardaboluk2@gmail.com>
2017-11-18 23:51:27 +0300

    Gamblers problem bugfix complete. Value function fits the book, lots of alternatives for policy.

--- sutton/GamblersProblem/gambler.c
+++ sutton/GamblersProblem/gambler.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define NUM_STATES 101
+#define NUM_STATES 100
 // the probability of the coin coming up heads
 #define P_H 0.4
 // the task is an undiscounted episodic task
 #define gamma 1
 
-#define abs(x) ((x) < 0 ? (-x) : (x))
-#define min(x,y) ((x) < (y) ? (x) : (y))
-#define max(x,y) ((x) > (y) ? (x) : (y))
-
 double values[NUM_STATES] = {0};
 int policy[NUM_STATES] = {0};
 
 // helper array for choosing max action
-double actionValues[NUM_STATES] = {0};
+double actionValues[50] = {0};
 
 void initPolicy();
 double getQ(int, int);
@@ -26,7 +23,7 @@ int chooseMaxAction(int);
 void writeValuesAndPolicy(char *, char *);
 
 void initPolicy(){
-  for(int s = 0; s < NUM_STATES; s++){
+  for(int s = 1; s < NUM_STATES; s++){
     policy[s] = rand() % NUM_STATES;
   }
 }
@@ -39,17 +36,12 @@ double getQ(int s, int a){
   int sprimeTails = s - a;
 
   int rewardHeads = 0;
-  int rewardTails = 0;
 
   if(sprimeHeads == 100){
     rewardHeads = 1;
   }
 
-  if(sprimeTails == 100){
-    rewardTails = 1;
-  }
-  
-  q = (1-P_H) * (rewardTails + gamma * values[sprimeTails]) + (P_H) * (rewardHeads + gamma * values[sprimeHeads]);
+  q = (1-P_H) * (gamma * values[sprimeTails]) + (P_H) * (rewardHeads + gamma * values[sprimeHeads]);
   
   return q;
 }
@@ -59,24 +51,24 @@ void valueIteration(){
   setbuf(stdout,NULL);
 
   double delta = 1000;
-  double epsilon = 0.001;
+  double epsilon = 0.000;
 
   int iterationCounter = 1;
 
   // find the value for each state
   while(delta > epsilon){
 
-    printf("Iteration %d delta %lf\n", iterationCounter, delta);
+    printf("Iteration %d delta %g\n", iterationCounter, delta);
 
     delta = 0;
 
-    for(int s = 0; s < NUM_STATES; s++){
+    for(int s = 1; s < NUM_STATES; s++){
 
       double temp = values[s];
 
       // find the maximum action value
       double maxActionValue = -1000;
-      for(int a = 0; a <= min(s, 100-s); a++){	
+      for(int a = 0; a <= (int)fmin(s, 100-s); a++){	
 	double newValue = getQ(s,a);
 	if(newValue > maxActionValue){
 	  maxActionValue = newValue;
@@ -85,9 +77,7 @@ void valueIteration(){
       
       values[s] = maxActionValue;
 
-      delta = max(delta, abs(temp-values[s]));
-
-      printf("State %d value: %lf\n", s, values[s]);
+      delta = fmax(delta, fabs(temp-values[s]));
     }
 
     iterationCounter++;
@@ -95,7 +85,7 @@ void valueIteration(){
 
   // find the policy for each state from the values
   printf("Finding the policy...\n");
-  for(int s = 0; s < NUM_STATES; s++){
+  for(int s = 1; s < NUM_STATES; s++){
 
     policy[s] = chooseMaxAction(s);
     printf("Policy for state %d: %d\n", s, policy[s]);
@@ -109,15 +99,10 @@ int chooseMaxAction(int s){
 
   int maxAction = 0;
 
-  int numActions = min(s,100-s);
+  int numActions = (int)fmin(s,100-s);
   double maxActionValue = -1000;
   // number of actions that has the maximum value
   int numMaxActions = 0;
-
-  // DEUG
-  if(s == 50){
-    int debug = 1;
-  }
   
   for(int a = 0; a <= numActions; a++){
     actionValues[a] = getQ(s,a);
@@ -155,15 +140,9 @@ void writeValuesAndPolicy(char * valuesFileName, char * policyFileName){
   fpv = fopen(valuesFileName, "w");
   fpp = fopen(policyFileName, "w");
 
-  for(int s = 0; s < NUM_STATES; s++){
-    if(s < 440){
+  for(int s = 1; s < NUM_STATES; s++){
       fprintf(fpv, "%lf,", values[s]);
-      fprintf(fpp, "%d,", policy[s]);
-    }
-    else{
-      fprintf(fpv, "%lf", values[s]);
-      fprintf(fpp, "%d", policy[s]);
-    }
+      fprintf(fpp, "%d,", policy[s]);    
   }
 
   fclose(fpv);
